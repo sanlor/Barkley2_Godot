ca5fffff06f4691c319914ac52267eb9cdff2ea3
sanlor <zpingusu@gmail.com>
2024-06-26 13:32:25 -0300

    Update r_title.gd
    migrating some stuff

--- barkley2/rooms/r_title.gd
+++ barkley2/rooms/r_title.gd
@@ -5,11 +5,15 @@ extends Control
 # This scene tries to recreate the room r_title, with the object oTitle.
 
 ## Godot Specific:
-# Load necessary Images
+
 #region Images
 
 @onready var bg = $bg
 const O_TITLE_STARPASS = preload("res://barkley2/scenes/sTitle/oTitleStarpass.tscn")
+
+#endregion
+
+# region Panels
 
 # Menu Panels
 var title_box
@@ -26,7 +30,114 @@ var settings_control_box
 var settings_gamepad_box # formely "dicks"
 var settings_get_key_box # for the key remap
 
+var stock_x := Array() # for the stock ticker
+
 #endregion
+
+# region Color stuff
+
+var text_color_normal := Color.GRAY # c_gray; // For text that cannot be interacted with
+var text_color_button := Color.LIGHTGRAY # c_ltgray; // Text that can be clicked
+var text_color_hover  := Color.WHITE # c_white;
+var text_color_select := Color.ORANGE # c_orange;
+
+var title_highlight_color := Color.ORANGE
+var settings_highlight_color := Color.ORANGE
+var gameslot_highlight_color := Color.ORANGE
+
+var key_highlight_color := Color(240, 50, 255) # make_color_rgb(240, 50, 255);
+
+#endregion
+
+# menu state
+var mode = "basic" :# "basic", "settings", "keymap", "gamepad", "gameslot","destruct_confirm", "gamestart_character" ## NOTE This reeeealy should be an enum.
+	set(_mode):
+		mode = _mode
+		change_menu() #automatically update the menu
+
+#endregion
+
+#region Bunch of stupid variables related to the menus layout
+
+## Basic title
+	var title_x = 142
+	var title_y = 170
+
+	var title_row = 16
+	var title_gap = 0
+
+	# Draw buttons
+
+	## Game slots
+	var gameslot_width = 344 - 8;
+	
+	var gameslot_x = 30;
+	var gameslot_y = 10; 
+	
+	var gameslot_row = 60 - 2;
+	var gameslot_gap = 10;
+
+
+	# Obliterate button
+	var gameslot_destruct_x = 244;
+	var gameslot_destruct_y = 214;
+
+	# back button
+	var gameslot_back_x = 40;
+	var gameslot_back_y = 214;
+
+	## Character Select ## "Create Character", "Play as X114JAM9", "Skip the Stupid Prologue", "Return"
+	var character_x = 20;
+	var character_y = 20;
+	var character_row = 20;
+	var character_gap = 5;
+
+	## Draw Settings
+	var settings_x = 40;
+	var settings_y = 64 - 6;
+	var settings_row = 16;
+	var settings_gap = 0;
+	var settings_width = (24 * 8);
+	var settings_option_x = 143 - 32;
+	
+	var settings_tab_y = settings_y - 48;
+	var settings_return_y = settings_y + settings_row * 9 + settings_gap * 9 + 0;
+
+	# Draw General
+	var slider_mx = 200; # Music
+	var slider_sx = 200; # Sfx
+	var slider_my = 40; # Music
+	var slider_sy = 60; # Sfx
+
+	# Draw Gamepad Menu
+
+	# Draw Keymap
+	var key_get = -1;
+	var key_lag = 0;
+	var key_x = 40;
+	var key_y = 64 - 6; ## 24;
+	var key_row = 16;
+	var key_gap = 0;
+	
+	## Stock Ticker ## Maybe this isnt usedin the "final" game?
+	stock_x[0] = 20;
+	stock_x[1] = 100;
+	stock_x[2] = 180;
+	stock_x[3] = 260;
+	stock_x[4] = 340;
+	stock_x[5] = 420;
+	stock_x[6] = 500;
+	stock_y = 225;
+
+	## Other garbage ##
+	var confirm_x = 132;
+	var confirm_y = 150;
+	var confirm_width = 50;
+	var confirm_gap = 20;
+
+#endregion
+
+var tim := 0.0 ## important variable. controles how much ot the ziggurat is near hell. range from 0 to 1
 
 func _ready():
 	for n in bg.get_children():
@@ -73,6 +184,15 @@ func _ready():
 
 	# Border("generate", 11, 256, 64); // Get key
 	settings_get_key_box = Border.generate(256, 64)
+
+	init_menus()
+	change_menu()
+
+func init_menus(): ## THis is just to alling with the old code. There are better ways to do this.
+	pass
+
+func change_menu():
+	pass
 
 func _input(event):
 	if event is InputEventMouseMotion:
