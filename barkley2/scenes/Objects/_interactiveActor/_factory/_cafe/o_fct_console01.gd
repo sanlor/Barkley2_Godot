6cb5d4ebabbaf54e4db1e1bf182f92c3746f161a
sanlor <zpingusu@gmail.com>
2024-10-13 22:27:31 -0300

    Issues related to the Wav 2 Ogg convertion

--- barkley2/scenes/Objects/_interactiveActor/_factory/_cafe/o_fct_console01.gd
+++ barkley2/scenes/Objects/_interactiveActor/_factory/_cafe/o_fct_console01.gd
@@ -7,7 +7,7 @@ var sound = "sn_machinemonitor01";
 var soundVolume = 0.15;
 
 func _ready() -> void:
-	var audio : AudioStreamWAV = load( B2_Sound.get_sound(sound) )
+	var audio : AudioStreamOggVorbis = load( B2_Sound.get_sound(sound) )
 	audio_stream_player_2d.stream = audio
 	audio_stream_player_2d.volume_db = linear_to_db(soundVolume)
 	audio_stream_player_2d.play()
--- barkley2/scenes/Objects/factory/floor2/o_fct_akiraSphere01.gd
+++ barkley2/scenes/Objects/factory/floor2/o_fct_akiraSphere01.gd
@@ -10,7 +10,7 @@ var soundVolume = 0.25;
 func _ready() -> void:
 	play("default")
 	
-	var audio : AudioStreamWAV = load( B2_Sound.get_sound(sound) )
+	var audio : AudioStreamOggVorbis = load( B2_Sound.get_sound(sound) )
 	audio_stream_player_2d.stream = audio
 	audio_stream_player_2d.volume_db = linear_to_db( soundVolume )
 	audio_stream_player_2d.play()
