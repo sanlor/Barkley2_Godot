ba141039d87c850fe4e6b06130032d3fa0a35f63
sanlor <zpingusu@gmail.com>
2025-10-24 08:57:04 -0300

    man...

--- barkley2/resources/B2_AI/ai.gd
+++ barkley2/resources/B2_AI/ai.gd
@@ -1,6 +1,10 @@
+## Base class for all AIs.
+# Contains some interfaces for a AI or the player to communicate to the Actor.
 @abstract
 extends Node
 class_name B2_AI
+
+# https://shaggydev.com/2023/10/08/godot-4-state-machines/
 
 signal ranged_attack_trigger( 	active : bool ) ## Actor is  firing ranged weapon
 signal melee_attack_trigger( 	active : bool ) ## Actor is attacking at melee range
@@ -12,11 +16,18 @@ signal jump_trigger(			active : bool ) ## Actor is Jumping
 var actor : B2_CombatActor
 var is_active := false
 
-@warning_ignore("unused_parameter")
+## AI Entered a new state
+func enter() -> void:
+	pass
+
+## AI exited a state
+func exit() -> void:
+	pass
+
+## Step function, executed on every process frame
 func step() -> void:
 	pass
 
 ## AI Action
-@warning_ignore("unused_parameter")
 func action() -> void:
 	push_warning("Invalid AI action")
--- barkley2/rooms/r_shader_loader.gd
+++ barkley2/rooms/r_shader_loader.gd
@@ -8,6 +8,8 @@ const SHADER_CODE 		:= "res://barkley2/shaders/shader_code/"
 
 const R_TITLE 			:= preload("uid://bpjwpt1mao0nm")
 
+const CACHE := ResourceLoader.CACHE_MODE_REUSE
+
 @onready var ball_sprite: Sprite2D 			= $ball_sprite
 @onready var loading_lbl: RichTextLabel 	= $MarginContainer/loading_lbl
 @onready var cuck_box: Control 				= $cuck_box
@@ -18,7 +20,9 @@ func _ready() -> void:
 	await get_tree().create_timer(0.5).timeout
 	print("%s: Loading shaders... %s" % [name, Time.get_time_string_from_system() ])
 	_load_particles()
+	await get_tree().create_timer(0.1).timeout
 	_load_materials()
+	await get_tree().create_timer(0.1).timeout
 	_load_gdshader()
 	print("%s: Finished! %s" % [name, Time.get_time_string_from_system() ])
 	await get_tree().create_timer(0.5).timeout
@@ -32,7 +36,7 @@ func _load_gdshader() -> void:
 			var part := ColorRect.new()
 			part.size = Vector2(50,50)
 			part.material = ShaderMaterial.new()
-			part.material.shader = ResourceLoader.load( SHADER_CODE + "/" + file, "", ResourceLoader.CACHE_MODE_REUSE )
+			part.material.shader = ResourceLoader.load( SHADER_CODE + "/" + file, "", CACHE )
 			cuck_box.add_child( part )
 			await get_tree().process_frame
 
@@ -42,7 +46,7 @@ func _load_materials() -> void:
 		if file.ends_with(".material") or file.ends_with(".tres"):
 			var part := ColorRect.new()
 			part.size = Vector2(50,50)
-			part.material = load( MATERIAL_FOLDER + "/" + file )
+			part.material = ResourceLoader.load( MATERIAL_FOLDER + "/" + file, "", CACHE )
 			cuck_box.add_child( part )
 			await get_tree().process_frame
 
@@ -52,14 +56,14 @@ func _load_particles() -> void:
 		if file.ends_with(".tres"):
 			var part := GPUParticles2D.new()
 			part.texture = preload("uid://ba1yave72rmvn")
-			part.process_material = load( PARTICLES_FOLDER + "/" + file )
+			part.process_material = ResourceLoader.load( PARTICLES_FOLDER + "/" + file, "", CACHE )
 			part.emitting = true
 			cuck_box.add_child( part )
 			await get_tree().process_frame
 		
 func _physics_process(delta: float) -> void:
 	t += 10.0 * delta
-	ball_sprite.scale.x = sin( t )
+	ball_sprite.scale.x = sin( t ) #speen
 	if ball_sprite.scale.x < 0.0: ball_sprite.modulate = 		Color.DARK_GRAY
 	else: ball_sprite.modulate = 								Color.WHITE
  
