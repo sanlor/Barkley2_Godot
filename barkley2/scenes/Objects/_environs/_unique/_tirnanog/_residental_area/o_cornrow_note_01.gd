90c8444f9979592201a6221ebfacd2321e8e51de
sanlor <zpingusu@gmail.com>
2025-12-30 09:56:33 -0300

    Cornrow adjustments

--- barkley2/scenes/Objects/_environs/_unique/_tirnanog/_residental_area/o_cornrow_note_01.gd
+++ barkley2/scenes/Objects/_environs/_unique/_tirnanog/_residental_area/o_cornrow_note_01.gd
@@ -2,8 +2,8 @@ extends B2_EnvironInteractive
 
 func execute_event_user_0(): 	
 	# Give gun
-	# scr_gun_db("juiceboxGun");
-	queue_free()
+	#scr_gun_db("juiceboxGun");
+	B2_Gun.append_gun_to_bandolier( B2_Gun.get_gun_from_db("juiceboxGun") )
 	
 func execute_event_user_1(): 	
 	is_interactive = false
